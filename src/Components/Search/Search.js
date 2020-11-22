0dfecd49388b2207ac8810d28f32cc6a9b4f2ceb
asad149 <asad.arshad.94849@gmail.com>
2020-11-22 02:25:54 -0800

    Date Picker Styling Completed

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -6,6 +6,8 @@ import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
 import { DateRangePicker } from "react-date-range";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
 // DATE PICKER COMPONENT
 
 const Search = () => {
@@ -26,6 +28,10 @@ const Search = () => {
   return (
     <div className="search">
       <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+      <h2>Number of guests  <PeopleIcon/></h2>
+     
+      <input min={0} defaultValue={2} type="number"/>
+      <Button>Search Airbnb</Button>
     </div>
   );
 };
