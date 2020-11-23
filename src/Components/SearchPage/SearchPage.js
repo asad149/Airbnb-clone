904c1caeb4654b419616d0c64149d436bff34477
asad149 <asad.arshad.94849@gmail.com>
2020-11-22 18:45:36 -0800

    Setup React Router in App.js

---
+++ src/Components/SearchPage/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SearchPage.css'
+const SearchPage = () => {
+    return (
+        <div className="searchPage">
+            
+        </div>
+    )
+}
+
+export default SearchPage
